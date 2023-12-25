f548caf9675369009fa9400d44dba60bd6af589b
Abhishek Musyuni <musyunia42@gmail.com>
2023-12-25 11:06:21 +0530

    changes

---
+++ src/ClassComponent/Footer.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div className='background p-3 mt-2 text-center'>
+                copyright@newsapp.com
+            </div>
+        )
+    }
+}
